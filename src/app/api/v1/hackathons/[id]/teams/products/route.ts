9b4284c22449d54eb008ccdd1f99c1169d4d9136
Kentaro1043 <71170923+Kentaro1043@users.noreply.github.com>
2025-03-06 22:31:01 +0900

    :sparkles: product

--- src/app/api/v1/hackathons/[id]/teams/products/route.ts
+++ src/app/api/v1/hackathons/[id]/teams/products/route.ts
@@ -4,56 +4,66 @@ import {PrismaClient} from '@prisma/client'
 const prisma = new PrismaClient()
 
 export async function GET(request: NextRequest, props: { params: Promise<{ id: string }> }) {
-  const params = await props.params;
+    const params = await props.params;
 
-  try {
-    const product = await prisma.product.findUnique({
-      where: {
-        id: Number(params.id),
-      },
-      select: {
-        id: true,
-        teamId: true,
-        title: true,
-        description: true,
-        createdAt: true,
-        updatedAt: true
-      }
-    });
+    try {
+        const teams = await prisma.team.findMany({
+            where: {
+                hackathonId: Number(params.id),
+            },
+            select: {
+                id: true
+            }
+        });
 
-    return NextResponse.json(product, {status: 200});
-  } catch (error) {
-    console.error("Error fetching products", error);
+        const products = await prisma.product.findMany({
+            where: {
+                teamId: {
+                    in: teams.map(team => team.id)
+                },
+            },
+            select: {
+                id: true,
+                teamId: true,
+                title: true,
+                description: true,
+                createdAt: true,
+                updatedAt: true
+            }
+        });
 
-    return NextResponse.json({body: "Failed to fetch products"}, {status: 500});
-  }
+        return NextResponse.json(products, {status: 200});
+    } catch (error) {
+        console.error("Error fetching products", error);
+
+        return NextResponse.json({body: "Failed to fetch products"}, {status: 500});
+    }
 }
 
-export async function POST(request: NextRequest, props: { params: Promise<{ id: string }> }) {
-  const params = await props.params;
-  const body = await request.json();
+export async function POST(request: NextRequest) {
+    const body = await request.json();
 
-  try {
-    const product = await prisma.product.create({
-      data: {
-        teamId: Number(params.id),
-        title: body.title,
-        description: body.description,
-      },
-      select: {
-        id: true,
-        teamId: true,
-        title: true,
-        description: true,
-        createdAt: true,
-        updatedAt: true
-      }
-    });
+    try {
+        const product = await prisma.product.create({
+            data: {
+                teamId: body.teamId,
+                title: body.title,
+                description: body.description,
+            },
+            select: {
+                id: true,
+                teamId: true,
+                title: true,
+                description: true,
+                createdAt: true,
+                updatedAt: true
+            }
+        });
 
-    return NextResponse.json(product, {status: 201});
-  } catch (error) {
-    console.error("Error creating product", error);
+        return NextResponse.json(product, {status: 201});
+    } catch (error) {
+        console.error("Error creating product", error);
 
-    return NextResponse.json({body: "Failed to create product"}, {status: 500});
-  }
+        return NextResponse.json({body: "Failed to create product"}, {status: 500});
+    }
 }
