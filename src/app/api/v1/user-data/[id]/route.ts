fa2b883dc837050581d78dac6236ae82aae65825
Kentaro1043 <71170923+Kentaro1043@users.noreply.github.com>
2025-03-06 18:11:33 +0900

    :sparkles: put userData

--- src/app/api/v1/user-data/[id]/route.ts
+++ src/app/api/v1/user-data/[id]/route.ts
@@ -51,3 +51,67 @@ export async function GET(request: NextRequest, props: { params: Promise<{ id: s
   }
 }
 
+export async function PUT(request: NextRequest, props: {
+  params: Promise<{ id: string }>,
+  body: Promise<{ affiliation: string, profile: string, userName: string, email: string }>
+}) {
+  const params = await props.params;
+  const body = await request.json();
+
+  try {
+    const userCheck = await prisma.user.findUnique({
+      where: {
+        id: params.id,
+      },
+      select: {
+        id: true,
+      }
+    });
+
+    if (!userCheck) {
+      return NextResponse.json({body: "User not found"}, {status: 404});
+    }
+
+    const user = await prisma.user.update({
+      where: {
+        id: params.id,
+      },
+      data: {
+        name: body.userName,
+        email: body.email,
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+      }
+    });
+
+    const userData = await prisma.userData.update({
+      where: {
+        userId: user.id,
+      },
+      data: {
+        affiliation: body.affiliation,
+        profile: body.profile,
+      },
+      select: {
+        userId: true,
+        affiliation: true,
+        profile: true
+      }
+    });
+
+    return NextResponse.json({
+      userId: user.id,
+      userName: user.name,
+      affiliation: userData.affiliation,
+      email: user.email,
+      profile: userData.profile
+    }, {status: 200});
+  } catch (error) {
+    console.error("Error updating user data", error);
+
+    return NextResponse.json({body: "Failed to update user data"}, {status: 500});
+  }
+}
