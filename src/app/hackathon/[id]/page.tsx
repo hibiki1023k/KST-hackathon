40c66f5b50679c0211a61e8a4c80709b699fecb1
KJ <gchojk1025@icloud.com>
2025-03-06 20:14:41 +0900

    update design

--- src/app/hackathon/[id]/page.tsx
+++ src/app/hackathon/[id]/page.tsx
@@ -7,6 +7,7 @@ import { CalendarIcon } from "lucide-react";
 import { dummyHackathonsdetails } from "./hackathon_dummy";
 import { useParams } from "next/navigation";
 import { useState } from "react";
+// import Image from "next/image";
 import {
   Dialog,
   DialogContent,
@@ -14,6 +15,8 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
+
+
 
 const HackathonPage = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -35,28 +38,39 @@ const HackathonPage = () => {
   }
 
   return (
-    <div className="max-w-2xl mx-auto p-4">
-      <div className="mb-4 p-4 text-center">
-        <h1 className="text-2xl font-bold">{hackathon.name}</h1>
-        <p className="text-blue-600 font-semibold">{hackathon.theme}</p>
-      </div>
+    <div>
 
-      <Card className="mb-4 p-4">
+      <div className="max-w-2xl mx-auto p-4">
+      <div className="mb-4 p-4 text-center">
+        {/* <Image
+          width={608}
+          height={200}
+          src=""
+          alt={hackathon.name}
+          className="flex justify-center object-cover rounded-lg"
+        /> */}
+        <h1 className="text-2xl font-bold">{hackathon.name}</h1>
+      </div>
+      </div>
+      <h1 className= "flex justify-center text-2xl font-bold ">テーマ</h1>
+
+      <h1 className= "flex justify-center text-4xl font-bold ">{hackathon.theme}</h1>
+      <div className="max-w-2xl mx-auto p-4">
+      <Dialog open={isOpen} onOpenChange={setIsOpen}>
+
+        <Card className="mb-4 p-4 text-center">
         <CardContent>
           <div className="flex flex-col space-y-4">
             <div className="flex items-center space-x-2">
               <CalendarIcon className="h-5 w-5" />
-              <p>開始日: {hackathon.startDate}</p>
+              <p >開始日: {hackathon.startDate}</p>
             </div>
             <div className="flex items-center space-x-2">
               <CalendarIcon className="h-5 w-5" />
-              <p>終了日: {hackathon.endDate}</p>
+              <p >終了日: {hackathon.endDate}</p>
             </div>
           </div>
         </CardContent>
-      </Card>
-      <Dialog open={isOpen} onOpenChange={setIsOpen}>
-        <Card className="mb-4 p-4 text-center">
           <Button 
             onClick={handleOnclick} 
             className="bg-blue-500 text-white w-full"
@@ -88,6 +102,7 @@ const HackathonPage = () => {
           </div>
         </DialogContent>
       </Dialog>
+      </div>
     </div>
   );
 };
